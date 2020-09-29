9e6b894d4a31028e0a6c87d91f2c41f7a8395da8
Pierre-Yves Dick <pyrrahnas@gmail.com>
2020-09-29 15:00:28 +0200

    Update PyrrahGravatarBundle.php

--- PyrrahGravatarBundle.php
+++ PyrrahGravatarBundle.php
@@ -6,13 +6,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle as BaseBundle;
 
 class PyrrahGravatarBundle extends BaseBundle
 {
-    public function getNamespace()
-    {
-        return __NAMESPACE__;
-    }
-
-    public function getPath()
-    {
-        return __DIR__;
-    }
 }
