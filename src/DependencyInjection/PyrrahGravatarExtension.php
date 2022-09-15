b77b8eccda6af0f476cd77c442d836e4bd952c3b
Pyrrah <pyrrahnas@gmail.com>
2022-09-15 17:20:11 +0200

    Remove lines

--- src/DependencyInjection/PyrrahGravatarExtension.php
+++ src/DependencyInjection/PyrrahGravatarExtension.php
@@ -9,26 +9,15 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * @author Pierre-Yves Dick
- *
- * @internal
  */
-class PyrrahGravatarExtension extends Extension
+final class PyrrahGravatarExtension extends Extension
 {
-    /**
-     * {@inheritdoc}
-     */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
-        foreach ($config as $name => $value) {
-            $container->setParameter(sprintf('pyrrah_gravatar.%s', $name), $value);
-        }
-
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new XmlFileLoader($container, new FileLocator(\dirname(__DIR__).'/Resources/config'));
         $loader->load('services.xml');
 
-        //$container->getDefinition('gravatar.api')->addArgument($config);
+        $configuration = $this->getConfiguration($configs, $container);
+        $config = $this->processConfiguration($configuration, $configs);
     }
 }
