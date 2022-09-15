5de720daabdaf25331b698c3b28bfe714a712634
Pyrrah <pyrrahnas@gmail.com>
2022-09-15 17:50:08 +0200

    Update PyrrahGravatarBundle.php

--- src/PyrrahGravatarBundle.php
+++ src/PyrrahGravatarBundle.php
@@ -2,19 +2,8 @@
 
 namespace Pyrrah\GravatarBundle;
 
-use Pyrrah\GravatarBundle\DependencyInjection\PyrrahGravatarExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PyrrahGravatarBundle extends Bundle
 {
-    /**
-     * Overridden to allow for the custom extension alias.
-     */
-    public function getContainerExtension()
-    {
-        if (null === $this->extension) {
-            $this->extension = new PyrrahGravatarExtension();
-        }
-        return $this->extension;
-    }
 }
